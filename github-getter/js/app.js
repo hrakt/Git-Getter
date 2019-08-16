bb3f72b6e2e7b95cebe2f1baf4b38d10610c08fb
Hrak <hrak@Hrak-Torousian.local>
2019-08-15 18:59:38 -0700

    first commit

--- github-getter/js/app.js
+++ github-getter/js/app.js
@@ -35,3 +35,58 @@
       }
     }
 */
+
+
+let search = document.getElementById("search");
+let results = document.getElementById("results-container");
+
+
+// search.addEventListener('submit', execute);
+
+execute = () => {
+  axios.get('https://api.github.com/search/repositories?q=' + search.value).then(function (response) {
+    const dataObject = response.data.items;
+    display(dataObject);
+    
+  }).catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+  });
+
+}
+
+display = (obj) => {
+  results.innerHTML = "";
+  obj.map(function(item, index) {
+ 
+
+    
+    results.innerHTML += create(item);
+  });
+  
+}
+
+create = (item) => {
+  let seperate = item.full_name.indexOf("/");
+  let author =  item.full_name.substr(0,seperate);
+  let repo =  item.full_name.substr(seperate + 1);
+
+  console.log(item);
+  let basicHTML = "<a href=`#`>" +  repo + " by " + author + "</a>" + "</br>";
+  let detailsHTML = `
+    <div class="details container"> 
+      <a>
+      Owner: ${item.owner.login}
+      Language: ${item.language} 
+      Forks: ${item.forks_count} 
+      Score: ${item.score} 
+      </a>  
+    </div>`;
+  let innerHTML = basicHTML + detailsHTML;
+  return innerHTML;
+}
+
+
+
+
